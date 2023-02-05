441fe55de76fca358666aee445b306dd2e50bc5b
SPY IV <109961542+Spy-IV@users.noreply.github.com>
2023-02-05 13:53:40 +0530

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,37 @@
+
+
+Config = {}
+Config.OpenPhone = 'f1'   --## Phone open key ## https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.Phones = {"phone"}  -- # item name
+Config.Locale = 'en'
+Config.Fahrenheit = false
+
+Config.ESXVersion = "1.2" -- 1.1 OR 1.2
+
+
+-- Discord WebHook - start --
+Config.Carseller = 'https://discord.com/api/webhooks/982923031213142087/pKSOMywHx66YHUWRJdXUJEuZsczLR6iasG5iy_t1k12qE0P4pCZKeoJ4Enpxhb1nyjZ6'
+Config.JobNotif = 'https://discord.com/api/webhooks/982923174238883860/k5cuDcbJpj4TFKaOnImm8sJCLHbzd4emhZ--s4zsMDG0Th7dBh402U1GW93wIC47hTtB'
+Config.TwitterWeb = 'https://discord.com/api/webhooks/982923241989500968/UwfOoJDSAo0oYwX87IRlekF2iScjIOyl3ESqF1AbhGQm4FHU9VJkEROnhOS02s-fvuNE'
+Config.YellowWeb = 'https://discord.com/api/webhooks/982923313389133884/rZTs7YlDYF6z8L3WfpLcKiJ7-3cMjb5JBfql5kqfvKOr6hHsMITsyWf9zL6JWqMEtNzG'
+Config.InstagramWeb = 'https://discord.com/api/webhooks/982923407253438484/vgrkvagwHdHGoAF3Sf0TYSP36t2d0WAFi2EeeHZKG5DONFoBe5pHyAtvxphVawd3gJ6A'
+Config.Crypto = 'https://discord.com/api/webhooks/982923507082084402/9PjrDc1ZCWzSK6Zmmqvi9Zp-iF2e3wlve8SyycJzAxaCqFd0qciGH7Q1JOkLGryYFkrp'
+Config.BankTrasnfer = 'Discord Webhook'
+Config.BankLimit = 50 -- # Minimum money transfer for discord webhook
+-- Discord WebHook - end --
+
+
+
+-- Phone Settings - Start --
+
+Config.UseMumbleVoIP    = true -- Use Frazzle's Mumble-VoIP Resource (Recomended!) https://github.com/FrazzIe/mumble-voip
+Config.PMAVoice         = false
+Config.UseTokoVoIP      = false
+Config.SaltyChat        = false
+
+Config.CallPhone        = true  -- If the player is not in the game or there is no item on it, it will give a warning.
+
+-- Phone Settings - Finish --
+
+Config.ValePrice        = 100    -- Vale Price
+Config.TaxiPrice        = 75     -- Taxi Price ( 75$/KM )
